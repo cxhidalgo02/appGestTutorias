0ace1cd9bd6798a14a9c0ed3ed63c3c4ec3562b2
cxhidalgo02 <cxhidalgo02@gmail.com>
2023-02-27 16:48:12 -0500

    Update AsignaturasDocenteScreen.js

--- src/screens/Docente/AsignaturasDocenteScreen.js
+++ src/screens/Docente/AsignaturasDocenteScreen.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { database } from '../../../config/';
+import { database } from '../../../config/firebaseConfig';
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import Asignaturas from '../../components/Asignaturas';
 import { StyleSheet, View, Text, SafeAreaView, Pressable } from 'react-native';
