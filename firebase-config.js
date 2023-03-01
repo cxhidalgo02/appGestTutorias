a7c0206e6d777b6ab2c4852a18a78069989912fa
cxhidalgo02 <cxhidalgo02@gmail.com>
2023-02-28 23:58:03 -0500

    update

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAO4Dlrpl7PeALj2dqukC1x5dw7NHzoTxw",
-    authDomain: "appgesttutorias.firebaseapp.com",
-    databaseURL: "https://appgesttutorias-default-rtdb.firebaseio.com",
-    projectId: "appgesttutorias",
-    storageBucket: "appgesttutorias.appspot.com",
-    messagingSenderId: "1017706378786",
-    appId: "1:1017706378786:web:b335cd06b9229bf4277c7b",
-    measurementId: "G-1R53FFB5ZF"
+  apiKey: "AIzaSyB2xt4xhFWRMaZcK46hN4VwsbJE-kMM1Zg",
+  authDomain: "databasefirebase-9a17e.firebaseapp.com",
+  projectId: "databasefirebase-9a17e",
+  storageBucket: "databasefirebase-9a17e.appspot.com",
+  messagingSenderId: "24528262519",
+  appId: "1:24528262519:web:7fa5719ab8efa2785cbd72"
   };
