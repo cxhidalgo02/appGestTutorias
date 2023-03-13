a103886a9c9a0d6bd85c77626f0cca3955a24546
cxhidalgo02 <cxhidalgo02@gmail.com>
2023-03-13 09:51:26 -0500

    Update tutorias js

--- src/components/Tutorias.js
+++ src/components/Tutorias.js
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import * as rn from 'react-native';
-//import { database } from '../src/fb';
-//import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
-//import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons'; 
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import { Entypo } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import localStorage from 'react-native-expo-localstorage';
 
 export default function Tutorias(
     {
@@ -19,34 +18,61 @@ export default function Tutorias(
         fechaCreacion,
     }
 ) {
+
+    const onDelete = () => {
+        const docRef = doc(database, 'asignaturaTutorias', id);
+        deleteDoc(docRef);
+    }
+
+    // Id del usuario que inicia sesion
+    const pathId = localStorage.getItem(`keyUser`, pathId);
+    // Id de la asignatura que seleccionar el usuario
+    const pathCodAsig = localStorage.getItem(`keyCodigo`, pathCodAsig);
+    // id de la tutoria que selecciona el usuario
+    const pathTutoria = `gestionUsuarios/${pathId}/asignaturas/${pathCodAsig}/tutorias/${id}`
+    const pathIdTut = localStorage.setItem("keyCodigoTut", id);
+    //const pathIdTutoria = localStorage.getItem(`keyCodigoTut`, id);
+
+    const [isDeleteActive, setIsDeleteActive] = React.useState(false);
     const navigation = useNavigation();
     return(
-        <rn.View style={styles.productContainer}>
-            
-            <rn.Text style={styles.title}> {tema} </rn.Text>
-            <rn.Text style={styles.descrip}>
-                <MaterialCommunityIcons name="watermark" size={18} color="black" /> - {descripcion} </rn.Text>
-            <rn.Text style={styles.information}> 
-                <Entypo name="chevron-right" size={18} color="black" /> Aula: {aula} </rn.Text>
-            <rn.Text style={styles.information}> 
-                <Entypo name="chevron-right" size={18} color="black" /> Hora: {hora} </rn.Text>
-            <rn.Text style={styles.information}>
-                <Entypo name="chevron-right" size={18} color="black" /> {semana} </rn.Text>
+        <rn.TouchableOpacity 
+            onLongPress={() => setIsDeleteActive(true)}
+            onPress={() => setIsDeleteActive(false)}
+            activeOpacity={0.8}
+        >
+            <rn.View style={styles.productContainer}>
+                <rn.Text style={styles.title}> {tema} </rn.Text>
+                <rn.Text style={styles.subtitle}> {id} </rn.Text>
+                <rn.Text style={styles.descrip}>
+                    <MaterialCommunityIcons name="watermark" size={18} color="black" /> - {descripcion} </rn.Text>
+                <rn.Text style={styles.information}> 
+                    <Entypo name="chevron-right" size={18} color="black" /> Aula: {aula} </rn.Text>
+                <rn.Text style={styles.information}> 
+                    <Entypo name="chevron-right" size={18} color="black" /> Hora: {hora} </rn.Text>
+                <rn.Text style={styles.information}>
+                    <Entypo name="chevron-right" size={18} color="black" /> {semana} </rn.Text>
 
-                <rn.View style={styles.btnsContiner}>
-                <rn.Pressable 
-                    onPress={() => alert('Lista de estudiantes que asistiran a la tutoria')}
-                    style={styles.btnContiner}>
-                    <FontAwesome5 name="user-check" size={25} color="black" />
-                </rn.Pressable>
+                    <rn.View style={styles.btnsContiner}>
+                    <rn.Pressable 
+                        onPress={() => alert('Lista de estudiantes que asistiran a la tutoria')}
+                        style={styles.btnContiner}>
+                        <FontAwesome5 name="user-check" size={25} color="black" />
+                    </rn.Pressable>
 
-                <rn.Pressable
-                    onPress={() => alert('Reporte de asistencia a la tutoria') }
-                    style={styles.btnContiner}>
-                    <FontAwesome5 name="file-download" size={25} color="black" />
-                </rn.Pressable>
+                    <rn.Pressable
+                        onPress={() => alert('Reporte de asistencia a la tutoria') }
+                        style={styles.btnContiner}>
+                        <FontAwesome5 name="file-download" size={25} color="black" />
+                    </rn.Pressable>
+                </rn.View>
             </rn.View>
-        </rn.View>
+            {isDeleteActive && (
+                <rn.Pressable onPress={onDelete} style={styles.deleteButton}>
+                <AntDesign name="delete" size={24} color="white" />
+                </rn.Pressable>
+            )}
+        </rn.TouchableOpacity>
     )
 }
 
@@ -65,6 +91,12 @@ const styles = rn.StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         color: '#293774',
+    },
+    subtitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#D4AC0D',
     },
     descrip: {
         fontSize: 16,
@@ -85,5 +117,15 @@ const styles = rn.StyleSheet.create({
         backgroundColor: 'transparent',
         alignItems: 'center',
     },
-
+    deleteButton: {
+        position: "absolute",
+        right: 8,
+        top: 0,
+        width: 40,
+        height: 40,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#293774",
+        borderRadius: 8,
+      },
 });
