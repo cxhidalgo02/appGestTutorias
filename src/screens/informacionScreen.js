0b13d9a3b0512ba8aa6f56ed9bc58cbea4dfa05a
cxhidalgo02 <cxhidalgo02@gmail.com>
2023-03-13 10:07:22 -0500

    Update Informacion Screen js

--- src/screens/informacionScreen.js
+++ src/screens/informacionScreen.js
@@ -14,8 +14,12 @@ const informacionScreen = ({ route, navigation })  => {
           <Text style={styles.textContent}>
               La aplicación esta desarrollada para el control y gestión de tutorias presenciales
           </Text>
+          
         </View>
+        <View style={styles.subcontainer2}>
         <ImageBackground source={image} style={styles.image}/>
+        </View>
+        
       </View>
     </SafeAreaView>
   );
@@ -24,45 +28,26 @@ export default informacionScreen;
 
 const styles = StyleSheet.create({
   image: {
-    width: 175,
-    height: 70,
-    marginLeft: 86,
+    width: '80%',
+    height: '50%',
   },
   container: {
     flex: 1, 
-    padding: 16,
     backgroundColor: '#FDFEFE',
   },
   subcontainer: {
     flex: 1, 
     alignItems: 'center', 
     justifyContent: 'center',
+    marginTop: 80,
+  },
+  subcontainer2: {
+    flex: 1, 
+    alignItems: 'flex-end',
   },
   textContent: {
     fontSize: 22, 
-    marginBottom: 16,
     textAlign: 'center', 
     color: '#293774',
-  },
-  textInput:{
-    borderWidth: 1,
-    borderColor: "#2E86C1",
-    backgroundColor:"#fff",
-    padding:10,
-    paddingStart: 20,
-    width: "80%",
-    marginTop:20,
-    borderRadius: 10,
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#2E86C1',
-    padding: 10,
-    width: "80%",
-    marginTop: 40,
-    borderRadius:10,
-  },
-  textbutton: {
-    color: "#F2F3F4",
   },
 });
