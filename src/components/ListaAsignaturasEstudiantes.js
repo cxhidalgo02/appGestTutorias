39c01a9ea2b70bad3916e6afb8c81dc183636e48
cxhidalgo02 <cxhidalgo02@gmail.com>
2023-03-13 09:48:57 -0500

    Update Asignaturas estudiantes js

--- src/components/ListaAsignaturasEstudiantes.js
+++ src/components/ListaAsignaturasEstudiantes.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import * as RN from 'react-native';
+import "react-native-gesture-handler";
+import { database } from '../../config/firebaseConfig';
 import { FontAwesome5 } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons'; 
 import { useNavigation } from '@react-navigation/native';
+import localStorage from 'react-native-expo-localstorage';
 
 export default function ListaAsignaturasEstudiantes({
     id,
@@ -10,42 +14,50 @@ export default function ListaAsignaturasEstudiantes({
     tipo,
     createdAt,
 }) {
-/*
+
     const onDelete = () => {
         const docRef = doc(database, 'asignaturaTutorias', id);
         deleteDoc(docRef);
     }
 
-    const onEdit = () => {
-        const docRef = doc(database, 'asignaturaTutorias', id);
-        updateDoc(docRef, {
-            isSold: true,
-        });
-    }
-*/
+    // Id del usuario que inicia sesion
+    const pathIdEst = localStorage.getItem(`keyUser`, pathIdEst);
+    //guardo el codigo de asignatura que seleecione
+    localStorage.setItem("keyCodigoEst", id);
 
-const navigation = useNavigation();
+    const [isDeleteActive, setIsDeleteActive] = React.useState(false);
+    const navigation = useNavigation();
     return(
-        <RN.View style={styles.productContainer}>     
-            <RN.Text style={styles.code}>{codigo}</RN.Text>
-            <RN.Text style={styles.textContent}>{nombre}</RN.Text>
-            <RN.Text style={styles.textContent}>{tipo}</RN.Text>    
-              
-            <RN.View style={styles.btnsContiner}>
+        <RN.TouchableOpacity 
+            onLongPress={() => setIsDeleteActive(true)}
+            onPress={() => setIsDeleteActive(false)}
+            activeOpacity={0.8}
+        >
+             <RN.View style={styles.productContainer} >     
+             <RN.Text style={styles.texttitle}>{nombre}</RN.Text>
+                <RN.Text style={styles.textsubtitle}>{codigo}</RN.Text>
+                <RN.Text style={styles.textContent}>Tipo: {tipo}</RN.Text>  
+                
+                <RN.View style={styles.btnsContiner}>
 
-                <RN.Pressable title='tutoriasEstudianteScreen'
-                    onPress={() => navigation.navigate('tutoriasEstudianteScreen')}
-                    style={styles.btnContiner}>
-                    <FontAwesome5 name="book" size={25} color="black" />
-                </RN.Pressable>
+                    <RN.Pressable title='tutoriasEstudianteScreen'
+                        onPress={() => navigation.navigate('tutoriasEstudianteScreen')}
+                        style={styles.btnContiner}>
+                        <FontAwesome5 name="swatchbook" size={25} color="black" />
+                    </RN.Pressable>
 
-                <RN.Pressable
-                    onPress={() => alert('Reporte semanal') }
-                    style={styles.btnContiner}>
-                    <FontAwesome5 name="file-download" size={25} color="black" />
-                </RN.Pressable>
+                    <RN.Pressable
+                        style={styles.btnContiner}>
+                        <FontAwesome5 name="file-download" size={25} color="black" />
+                    </RN.Pressable>
+                </RN.View>
             </RN.View>
-        </RN.View>
+            {isDeleteActive && (
+                <RN.Pressable onPress={onDelete} style={styles.deleteButton}>
+                <AntDesign name="delete" size={24} color="white" />
+                </RN.Pressable>
+            )}
+        </RN.TouchableOpacity>
     )
 }
 
@@ -59,14 +71,21 @@ const styles = RN.StyleSheet.create({
         borderColor: "#2E86C1",
         backgroundColor:"#fff",
     },
-    code: {
+    texttitle: {
         fontSize: 18,
         fontWeight: 'bold',
+        textAlign: 'center',
         color: '#293774',
-        padding: 5,
+        
+    },
+    textsubtitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#D4AC0D',
     },
     textContent:{
-        fontSize: 18,
+        fontSize: 16,
+        color: '#293774',
     },
     button: {
         backgroundColor: '#0FA5E9',
@@ -81,7 +100,7 @@ const styles = RN.StyleSheet.create({
         color: '#fff',
     },
     btnsContiner:{
-        width: '90%',
+        width: '100%',
         backgroundColor: 'transparent',
         flexDirection: "row",
     },
@@ -92,4 +111,27 @@ const styles = RN.StyleSheet.create({
         backgroundColor: 'transparent',
         alignItems: 'center',
     },
+    /* MODAL */
+    contentContainer: {
+        flex: 1,
+        alignItems: "center",
+        paddingHorizontal: 15,
+    },
+    description: {
+        color: "#56636F",
+        fontSize: 13,
+        fontWeight: "normal",
+        width: "100%",
+      },
+      deleteButton: {
+        position: "absolute",
+        right: 8,
+        top: 0,
+        width: 40,
+        height: 40,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#293774",
+        borderRadius: 8,
+      },  
 });
