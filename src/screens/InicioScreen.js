c7981ffc3797ad9cc4848085d6294ea2c36668e2
cxhidalgo02 <cxhidalgo02@gmail.com>
2023-03-27 14:08:36 -0500

    update

--- src/screens/InicioScreen.js
+++ src/screens/InicioScreen.js
@@ -53,10 +53,12 @@ const InicioScreen = ({ navigation })=> {
         switch(tipo){
           case "Docente":
             navigation.navigate('asignaturasDocenteScreen');
-            localStorage.setItem("keyUser", userUid);
+            //localStorage.setItem("keyUser", userUid);
+            localStorage.setItem("keyUserDoc", userUid);
           case "Estudiante":
             navigation.navigate('asignaturasEstudiantesScreen');
-            localStorage.setItem("keyUser", userUid);
+            //localStorage.setItem("keyUser", userUid);
+            localStorage.setItem("keyUserEst", userUid);
              break;
         }
        }
