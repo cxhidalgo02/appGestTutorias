70734343c6f362e67eddd849a7e5f912f6d857a0
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-04-05 20:24:46 -0500

    update skeleton validar estudiante

--- src/screens/Docente/ValidarAsistenciaScreen.js
+++ src/screens/Docente/ValidarAsistenciaScreen.js
@@ -5,6 +5,8 @@ import ValidarAsistencia from '../../components/ValidarAsistencia';
 import { StyleSheet, View, Text, SafeAreaView, RefreshControl } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import localStorage from 'react-native-expo-localstorage';
+import { Skeleton } from 'moti/skeleton'; 
+import { MotiView } from 'moti';
 
 const ValidarAsistenciaScreen = () => {
 
@@ -43,19 +45,36 @@ const onRefresh = React.useCallback(() => {
   }, 2000);
 }, []);
 
+const Spacer =  ({height = 25}) => <MotiView style={{height}}/>
+function MySkeleton() {
   return (
+    <>
+      <Skeleton width={'80%'} height={135} colorMode={'light'} />
+      <Spacer/>
+      <Skeleton width={'80%'} height={135} colorMode={'light'} />
+      <Spacer/>
+      <Skeleton width={'80%'} height={135} colorMode={'light'} />
+      <Spacer/>
+      <Skeleton width={'80%'} height={135} colorMode={'light'} />
+    </>
+  );
+}
+
+return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container} >
         <Text style={styles.textTitle}>
             VALIDAR ASISTENCIA
           </Text>
-          <ScrollView style={styles.scrollAsig}
-            refreshControl={
-              <RefreshControl refreshing ={refreshing} onRefresh={onRefresh}/>
-            } 
-          >
-            {estudiante.map(estudiante=> <ValidarAsistencia key={estudiante.id} {...estudiante}/>)}
-          </ScrollView>
+          <React.Suspense fallback={<MySkeleton />}>
+            <ScrollView style={styles.scrollAsig}
+              refreshControl={
+                <RefreshControl refreshing ={refreshing} onRefresh={onRefresh}/>
+              } 
+            >
+              {estudiante.map(estudiante=> <ValidarAsistencia key={estudiante.id} {...estudiante}/>)}
+            </ScrollView>
+          </React.Suspense>
       </View>
     </SafeAreaView>
   );
