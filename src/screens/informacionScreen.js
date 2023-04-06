551f8ec5c7d006f9899e040661f634750a2815fa
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-04-05 19:58:53 -0500

    update skeleton

--- src/screens/informacionScreen.js
+++ src/screens/informacionScreen.js
@@ -1,20 +1,39 @@
 import * as React from 'react';
 import { StyleSheet, View, Text, SafeAreaView, ImageBackground } from 'react-native';
 import { AntDesign } from '@expo/vector-icons'; 
+import { Skeleton } from 'moti/skeleton'; 
+import { MotiView } from 'moti';
 
 const image = {uri: 'https://www.utpl.edu.ec/sites/default/files/archivos/marca%20UTPL%202018-03.png'};
+
+const Spacer =  ({height = 25}) => <MotiView style={{height}}/>
+
+function MySkeleton() {
+  return (
+    <>
+      <Skeleton width={'10%'} height={40} colorMode={'light'} />
+      <Spacer/>
+      <Skeleton width={'80%'} height={150} colorMode={'light'} />
+      <Spacer/>
+      <Skeleton width={'60%'} height={80} colorMode={'light'} />
+    </>
+  );
+}
 
 const informacionScreen = ({ route, navigation })  => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container} >
         <View style={styles.subcontainer}>
-          <AntDesign name="appstore-o" size={35} color="#293774" style={{marginBottom: 25}} />
 
-          <Text style={styles.textContent}>
-              La aplicación esta desarrollada para el control y seguimiento 
-              de tutorias a los estudiantes en la modalidad presencial.
-          </Text>
+          <React.Suspense fallback={<MySkeleton />}>
+            <AntDesign name="appstore-o" size={35} color="#293774" style={{marginBottom: 25}} />
+
+            <Text style={styles.textContent}>
+                La aplicación esta desarrollada para el control y seguimiento 
+                de tutorias a los estudiantes en la modalidad presencial.
+            </Text>
+          </React.Suspense>
           
         </View>
         <View style={styles.subcontainer2}>
