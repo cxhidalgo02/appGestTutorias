79dae01ffcd0b3558fb5d4deae3d40d4178bcaf9
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-04-06 20:59:36 -0500

    update styles

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export const myColors = {
+    light: '#F1F2F3',
+    dark: '#17171C',
+        
+    black: '#000000', //negro
+    white: '#FFFFFF', //blanco
+    gray: '#747477', //gris
+    navyblue : '#293774', //azul marino
+    blue: '#4B5EFC', //azul
+    skyblue : '#2E86C1', //celeste
+    mustard: '#D4AC0D', //mostaza
+    green: '#0E6655', //verde
+}
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,34 +1,42 @@
-const styles = StyleSheet.create({
-    container: {
-      flex: 1, 
-      padding: 16,
-      alignItems: 'center', 
-      justifyContent: 'center',
-    },
-    textTitle: {
-      fontSize: 22, 
-      textAlign: 'center', 
-      marginBottom: 16, 
-    },
-    scrollForm: {
-      textAlign: "center",
-    },
-    textInput:{
-      borderWidth: 1,
-      borderColor: "#2E86C1",
-      backgroundColor:"#fff",
-      padding:10,
-      marginTop:20,
-      borderRadius: 10,
-    },
-    button: {
-      alignItems: 'center',
-      backgroundColor: '#2E86C1',
-      padding: 10,
-      marginTop: 40,
-      borderRadius:10,
-    },
-    textbutton: {
-      color: "#F2F3F4",
-    },
+import { StyleSheet } from "react-native";
+import { myColors } from "./colors";
+
+export const style = StyleSheet.create({
+  container: {
+    flex: 1, 
+    padding: 16,
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+  textTitle: {
+    fontSize: 20, 
+    textAlign: 'center', 
+    marginBottom: 16, 
+    color: myColors.navyblue,
+  },
+  textContent: {
+    fontSize: 22, 
+    textAlign: 'center', 
+    color: '#293774',
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: myColors.skyblue,
+    backgroundColor: myColors.white,
+    padding:10,
+    paddingStart: 20,
+    marginTop:20,
+    borderRadius: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: myColors.navyblue,
+    padding: 10,
+    marginTop: 40,
+    borderRadius:10,
+    elevation: 5,
+  },
+  textbutton: {
+    color: myColors.light,
+  },
   });
