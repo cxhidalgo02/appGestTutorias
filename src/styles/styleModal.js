592787dca288d04acb125e5c706ad0c262fca08f
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-04-11 17:38:45 -0500

    create styles modales

---
+++ src/styles/styleModal.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+import { myColors } from "./colors";
+
+export const styleModal = StyleSheet.create({
+//VENTANA MODAL
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        width: '100%',
+        height: '80%',
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: myColors.white,
+        borderRadius: 10,
+        padding: 40,
+        alignItems: 'center',
+        shadowColor: myColors.black,
+        shadowOffset: {
+        width: 0,
+        height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    buttonClose: {
+        marginTop: 20,
+        borderRadius: 8,
+        padding: 10,
+        elevation: 2,
+        backgroundColor: myColors.mustard,
+    },
+    textButtonClose: {
+        color: myColors.white,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalTextTitle: {
+        marginBottom: 15,
+        color: myColors.navyblue,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        fontSize: 16,
+        color: myColors.navyblue,
+        textAlign: 'center',
+        padding: 8,
+    },
+
+  });
