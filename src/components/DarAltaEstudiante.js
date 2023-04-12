7486b4b725e3fa59b4c3f8bc1e70059a7a00080d
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-04-12 17:40:09 -0500

    update

--- src/components/DarAltaEstudiante.js
+++ src/components/DarAltaEstudiante.js
@@ -1,21 +1,18 @@
 import * as React  from 'react';
 import * as rn from 'react-native';
-import { database } from '../../config/firebaseConfig';
-import { doc, updateDoc, Firestore, collection, query, where, getDocs, setDoc } from 'firebase/firestore';
+import { styleComp } from '../styles/stylesComp';
 import { FontAwesome5 } from '@expo/vector-icons'; 
 import { MaterialIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { database } from '../../config/firebaseConfig';
+import { doc, updateDoc, collection, query, where, getDocs, setDoc } from 'firebase/firestore';
+
 import localStorage from 'react-native-expo-localstorage';
 
-export default function DarAltaEstudiante(
-    {
-        id,
-        cedula,
-        nombres,
-        apellidos,
-        correo,
-    }
-) {
+export default function DarAltaEstudiante({
+        id,cedula, nombres, apellidos, correo,
+    }) 
+{
     //Uid del estudiante que encuentre en la base de datos   (id) 
     const pathIdEstData = localStorage.setItem("keyUserEstData", id); //console.log('UID del estudiante = ', id);
     //Uid del docente que inicia sesion   (id) 
@@ -111,7 +108,7 @@ export default function DarAltaEstudiante(
             onPress={() => setIsValidateActive(false)}
             activeOpacity={0.8}
             >
-            <rn.View style={styles.productContainer}>
+            <rn.View style={styleComp.productContainer}>
                 <rn.Text> 
                     <MaterialIcons name="fingerprint" size={18} color="black" /> - {cedula} 
                 </rn.Text>
@@ -122,47 +119,11 @@ export default function DarAltaEstudiante(
                     <MaterialIcons name="mail" size={18} color="black" /> - {correo} 
                 </rn.Text>
             </rn.View>
-            
             {isValidateActive && (
-                <rn.Pressable onPress={onValidate} style={styles.validateButton}>
+                <rn.Pressable onPress={onValidate} style={styleComp.validateButton}>
                     <AntDesign name="checksquareo" size={24} color="white" />
                 </rn.Pressable>
             )}
-                
         </rn.TouchableOpacity>
     )
 }
-
-const styles = rn.StyleSheet.create({
-    productContainer: {
-        width: "85%",
-        padding: 10,
-        margin: 15,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: "#2E86C1",
-        backgroundColor:"#fff",
-        elevation: 5,
-    },
-    validateButton: {
-        position: "absolute",
-        right: 8,
-        width: 40,
-        height: 40,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#D4AC0D",
-        borderRadius: 8,
-    },
-    deleteButton: {
-        position: "absolute",
-        right: 8,
-        top: 50,
-        width: 40,
-        height: 40,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#293774",
-        borderRadius: 8,
-      },
-});
