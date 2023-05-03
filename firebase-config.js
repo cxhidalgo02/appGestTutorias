6d88afdb48ec28841288c7ef34788a1f37d918d2
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-05-03 10:22:15 -0500

    update credentials firebase
    update configurations credentials firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDOYzrFZbgV_esj5GTGae57BTJZ9Mopg3c",
-  authDomain: "datafirebase-a1747.firebaseapp.com",
-  projectId: "datafirebase-a1747",
-  storageBucket: "datafirebase-a1747.appspot.com",
-  messagingSenderId: "45438180320",
-  appId: "1:45438180320:web:86a7fee80063e1bca3e985"
+  apiKey: "AIzaSyAVPprajgJGQqxDToCTVJMIU8-xJ_0awfg",
+  authDomain: "appgesttutorias-23149.firebaseapp.com",
+  projectId: "appgesttutorias-23149",
+  storageBucket: "appgesttutorias-23149.appspot.com",
+  messagingSenderId: "315072874555",
+  appId: "1:315072874555:web:51ca4036eb60e2e1fe341e",
+  measurementId: "G-K0M5RD861K"
   };
