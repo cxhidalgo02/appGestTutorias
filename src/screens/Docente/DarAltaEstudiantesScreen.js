aeb7ef3a19e8c785723f67570769752e73aee9c3
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-05-25 23:22:43 -0500

    update docente
    update docente dar de alata

--- src/screens/Docente/DarAltaEstudiantesScreen.js
+++ src/screens/Docente/DarAltaEstudiantesScreen.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { style } from '../../styles/styles';
 import { database } from '../../../config/firebaseConfig';
 import { collection, onSnapshot, query, where,} from 'firebase/firestore';
-import localStorage from 'react-native-expo-localstorage';
+//import localStorage from 'react-native-expo-localstorage';
 import DarAltaEstudiante from '../../components/DarAltaEstudiante';
 import { View, Text, SafeAreaView, RefreshControl } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -12,11 +12,11 @@ const DarAltaEstudiantesScreen = () => {
 
   const [estudiante, setNuevoEstudiante] = React.useState([]);
   // Id del usuario que inicia sesion
-  const pathIdDoc = localStorage.getItem(`keyUserDoc`, pathIdDoc);
+  //const pathIdDoc = localStorage.getItem(`keyUserDoc`, pathIdDoc);
   // Id del estudiante que se trae de dar de alata estudiante screen
-  const pathIdEstData = localStorage.getItem(`keyUserEstData`, pathIdEstData);
+  //const pathIdEstData = localStorage.getItem(`keyUserEstData`, pathIdEstData);
   // Id de la asignatura que seleccionar el usuario
-  const pathIdAsig = localStorage.getItem(`keyCodAsigDoc`, pathIdAsig);
+  //const pathIdAsig = localStorage.getItem(`keyCodAsigDoc`, pathIdAsig);
 
   async function consultaEstudiantes() {
     try {
