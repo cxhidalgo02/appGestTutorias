afd2a52a4bf92b48f44361f0d7c1980fc370d7d9
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-06-18 20:48:56 -0500

    update informacion screen
    update informacion screen, delete image

--- src/screens/informacionScreen.js
+++ src/screens/informacionScreen.js
@@ -1,56 +1,76 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, SafeAreaView, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 import { style } from '../styles/styles'; 
 import { myColors } from '../styles/colors';
 import { Octicons } from '@expo/vector-icons';  
 
-const image = {uri: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Universidad_T%C3%A9cnica_Particular_de_Loja.png'};
-
 const informacionScreen = ()  => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={style.container} >
-        <View style={styles.subcontainer} >
-          <View style={styles.containerImg}>  
-            <ImageBackground source={image} style={styles.image}/>  
-          </View>
-          <View>
-            <Text style={style.textContent}>
-              La aplicación esta desarrollada para el control y seguimiento 
-              de tutorias en la modalidad presencial.
-            </Text>
-          </View>
+      <View style={styles.container} >
+        <View style={styles.title} >
+          <Text style={styles.textTitle}>
+            Gestión de Tutorias
+          </Text>
+        </View> 
+        <View style={styles.content}>
+          <Text style={styles.textContent}>
+            La aplicación está dirigida a docentes y estudiantes, específicamente para el control y seguimiento de tutorías en 
+            entornos presenciales.
+          </Text>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.textMarca}>
+            UTPL
+          </Text>
+          <Octicons name="versions" size={24} style={styles.icon} />
+          <Text style={style.textFooter}>
+            Versión: 1.0.0
+          </Text>
         </View>
       </View>
-      <Octicons name="versions" size={24} style={styles.icon} />
-      <Text style={style.textFooter}>
-        Versión: 1.0.0
-      </Text>
     </SafeAreaView>
   );
 };
 export default informacionScreen;
 
 const styles = StyleSheet.create({
-  image: {
-    marginTop: 50,
-    marginBottom: 60,
-    marginLeft: 85,
-    width: '60%', // Ajusta el ancho de la imagen según tus necesidades
-    aspectRatio:1, // Mantiene la proporción original de la imagen
+  container: {
+    width: '100%',
+  },
+  title:{
+    marginTop: 120,
+  },
+  content: {
+    marginTop: 20,
+  },
+  footer: {
+    marginTop: 180,
+  },
+  textTitle: {
+    fontSize: 24, 
+    textAlign: 'center', 
+    fontWeight: 'bold',
+    color: myColors.mustard,
+    padding: 20,
+    
+  },
+  textContent: {
+    fontSize: 19, 
+    textAlign: 'center', 
+    color: myColors.navyblue,
+    padding: 30,
+  },
+  textMarca: {
+    fontSize: 24, 
+    textAlign: 'center', 
+    fontWeight: 'bold',
+    color: myColors.navyblue,
+    padding: 20,
+    marginBottom: 20,
   },
   icon: {
-    marginTop: 200,
     textAlign: 'center',
     color : myColors.navyblue,
-  },
-  subcontainer: {
-    //width: '80%',
-    justifyContent: 'center',
-    alignItems: 'center', 
-  },
-  containerImg: {
-    justifyContent: 'center',
-    alignItems: 'center', 
   },
 });
