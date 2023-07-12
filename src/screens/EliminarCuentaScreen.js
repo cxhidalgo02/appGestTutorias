3dd801730b9a724331deca86f6dffcb445bcefe4
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-07-12 09:45:50 -0500

    Create Eliminar cuenta
    Create Eliminar cuenta screen

---
+++ src/screens/EliminarCuentaScreen.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react';
+import { TouchableOpacity, StyleSheet, View, Text,Modal, TextInput, Pressable } from 'react-native';
+import { style } from '../styles/styles';
+import { styleModal } from '../styles/styleModal';
+import Layout from '../components/layout/Layout';
+
+const EliminarCuenta = ({ navigation })=> {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+return (
+  <Layout>
+    <Text style={style.textTitle}>
+      CONFIRMAR CORREO
+    </Text>
+    <View style={styles.subcontainerText}>
+      <Text style={style.textContentt}>
+        Ingrese su correo para confirmar la eliminación de la cuenta.
+      </Text>
+    </View>
+    <TextInput
+      style={[style.textInput, Platform.OS === 'ios' && style.iOS_textInput]}
+      placeholder="Correo"
+      textContentType="emailAddress"
+      autoCapitalize='none'/>
+      <TouchableOpacity style={style.button} onPress={() => setModalVisible(true)}>
+        <Text style={style.textbutton}>ENVIAR</Text>
+      </TouchableOpacity>
+
+      <View style={styleModal.centeredView}>
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+            setModalVisible(!modalVisible);
+            }}>
+            <View style={styleModal.centeredView}>
+                <View style={styleModal.modalView}>
+                    <Text style={styleModal.modalTextTitle}>IMPORTANTE!</Text>
+                    <Text style={styleModal.modalText}>
+                      Pronto nos contacteremos para finalizar el proceso de eliminacion de la cuenta, 
+                      mientras tanto sigue disfrutando de nuestro servicio.
+                    </Text>
+                    <Pressable
+                        style={styleModal.buttonClose}
+                        onPress={() => setModalVisible(!modalVisible)}>
+                        <Text style={styleModal.textButtonClose}>CERRAR</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </Modal>
+      </View>
+  </Layout>
+
+
+);
+};
+export default EliminarCuenta;
+
+const styles = StyleSheet.create({
+  subcontainer: {
+    width: '75%',
+    marginTop: 175,
+  },
+  subcontainerText: {
+    marginTop: 40,
+  },
+});
