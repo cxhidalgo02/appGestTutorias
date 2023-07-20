e96d71df7895150a36304b8046f64b422db624f9
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-07-20 13:25:05 -0500

    update colors
    update cstyle olors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,13 +1,14 @@
 export const myColors = {
-    light: '#F1F2F3',
-    dark: '#17171C',
-        
+    light: '#F1F2F3', // gris claro
     black: '#000000', //negro
     white: '#FFFFFF', //blanco
-    gray: '#747477', //gris
+    gray: '#D0D3D4', //gris
     navyblue : '#293774', //azul marino
-    blue: '#4B5EFC', //azul
     skyblue : '#2E86C1', //celeste
     mustard: '#D4AC0D', //mostaza
-    green: '#0E6655', //verde
+    dark_green: '#0E6655', //verde
+    red: '#FF0000', //rojo
+    blue: '#0000FF', //azul
+    green: '#00FF00', //verde
+    yellow: '#FFFF00', //amarillo
 }
