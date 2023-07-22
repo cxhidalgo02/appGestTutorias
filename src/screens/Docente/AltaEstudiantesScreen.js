994d7e3dc060b1efda48fe158ab4025389096b14
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-07-22 00:58:44 -0500

    update validar acceso screen
    update validar acceso screen

--- src/screens/Docente/AltaEstudiantesScreen.js
+++ src/screens/Docente/AltaEstudiantesScreen.js
@@ -3,15 +3,14 @@ import { style } from '../../styles/styles';
 import { database } from '../../../config/firebaseConfig';
 import { collection, onSnapshot, query, where,} from 'firebase/firestore';
 import localStorage from 'react-native-expo-localstorage';
-import { View, Text, SafeAreaView, RefreshControl } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Text, View } from 'react-native';
 import AltaEstudiante from '../../components/Docente/AltaEstudiante';
 import Layout from '../../components/layout/Layout';
 
 const AltaEstudiantesScreen = () => {
 
   //constructor de estudiante para la clase dar de alata estudiante
-  const [estudiante, setNuevoEstudiante] = React.useState([]);
+  const [listaAltaEst, setListaAltaEst] = React.useState([]);
   // Id del usuario que inicia sesion
   const pathIdDoc = localStorage.getItem(`keyUserDoc`, pathIdDoc);
   // Id del estudiante que se trae de dar de alata estudiante screen
@@ -21,23 +20,59 @@ const AltaEstudiantesScreen = () => {
 
   React.useEffect(() => {
     try {
-      const collectionRef = collection(database, 'Usuarios');
-      const qOne = query(collectionRef, where("tipo", "==", "Estudiante") );
-      const unsubscribe2 = onSnapshot(qOne, querySnapshot => {
-          setNuevoEstudiante(
-              querySnapshot.docs.map(doc => ({
+      // CONSULTA ESTUDIANTES
+      const q1 = query(collection(database, 'Usuarios'), where("tipo", "==", "Estudiante"));
+      const unsubscribe = onSnapshot(q1, querySnapshot => {
+        const estudiantesData = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          cedula: doc.data().cedula,
+          nombres: doc.data().nombres,
+          apellidos: doc.data().apellidos,
+          correo: doc.data().correo,
+        }));
+        // Realizar la consulta de asignaturas para cada estudiante
+        estudiantesData.forEach(estudiante => {
+          const uid_Estudiante = estudiante.id;
+          const cedula_Estudiante = estudiante.cedula;
+          // CONSULTA REFERENCIA ESTUDIANTES
+          const q2 = query(collection(database, `Usuarios/${uid_Estudiante}/AsignaturasEstudiante/`), 
+                      where("codigoAsig", "==", pathIdAsig)
+          );
+          const unsubscribe2 = onSnapshot(q2, querySnapshot => {
+            const asignaturaData = querySnapshot.docs.map(doc => ({
+              id: doc.id,
+              nombreAsig: doc.data().nombreAsig,
+              altaAsigEst : doc.data().altaAsigEst,
+            }));
+            // Se guardda el codigo de las asignatura seleecionada
+            const codigo_asig = asignaturaData[0]?.id;
+            const nom_asig = asignaturaData[0]?.nombreAsig;
+            const alta_asig = asignaturaData[0]?.altaAsigEst;
+            
+            if( codigo_asig === pathIdAsig && alta_asig === 'false' && nom_asig === '' ){
+              console.log('Hay que dar acceso!', codigo_asig, ' ', alta_asig, ' ', nom_asig, '');
+              const q3 = query(collection(database, 'Usuarios'), where("cedula", "==", cedula_Estudiante));
+              const unsubscribe3 = onSnapshot(q3, querySnapshot => {
+                const estudiantesNewData = querySnapshot.docs.map(doc => ({
                   id: doc.id,
                   cedula: doc.data().cedula,
                   nombres: doc.data().nombres,
                   apellidos: doc.data().apellidos,
                   correo: doc.data().correo,
-              }))
-            );
+                }));
+                 // Agregar todos los estudiantes a listaAltaEst utilizando concat()
+                setListaAltaEst(prevLista => prevLista.concat(estudiantesNewData));
+                //console.log('Data estudiante > ', estudiantesNewData);
+              });
+            }
           });
-      return unsubscribe2;
+        });
+        return unsubscribe;
+      });
     } catch (error) {
       console.log('ERROR =>', error);
     }
+
   },[])
 
   //estados para refrezcar el screen
@@ -51,10 +86,12 @@ const AltaEstudiantesScreen = () => {
 
   return (
     <Layout>
-      <Text style={style.textTitle}>
-        VALIDAR ACCESO
-      </Text>
-        {estudiante.map(estudiante=> <AltaEstudiante key={estudiante.id} {...estudiante}/>)}
+      <View style={style.titleContainer}>
+        <Text style={style.textTitle}>
+          VALIDAR ACCESO
+        </Text>
+      </View>
+        {listaAltaEst.map(listaAltaEst=> <AltaEstudiante key={listaAltaEst.id} {...listaAltaEst}/>)}
     </Layout>
   );
 };
