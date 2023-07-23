f1aac9875a8e9b40f723276964dccb47f5f83f19
Carlos Hidalgo <cxhidalgo02@gmail.com>
2023-07-23 12:37:03 -0500

    update componente docente
    update NumEstudiantesInscritosData y numTutoriasValidadasData

--- src/components/Docente/Tutorias.js
+++ src/components/Docente/Tutorias.js
@@ -7,6 +7,7 @@ import { AntDesign } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons'; 
 import { Ionicons } from '@expo/vector-icons'; 
 import { useNavigation } from '@react-navigation/native';
+import { collection, query,  getCountFromServer, onSnapshot, where, getDocs } from 'firebase/firestore';
 import localStorage from 'react-native-expo-localstorage';
 import { doc, deleteDoc} from 'firebase/firestore';
 
@@ -22,6 +23,10 @@ export default function Tutorias({
     const pathIdAsigDoc = localStorage.getItem(`keyCodAsigDoc`, pathIdAsigDoc);
     // id de la tutoria que selecciona el usuario
     const pathIdTutDoc = localStorage.setItem("keyCodTutDoc", id);
+      //codigo de las asignatura de seleccione
+    const pathIdAsig = localStorage.getItem(`keyCodAsigDoc`, pathIdAsig);
+      //tutoria seleccionada
+    const pathCodTutDoc = localStorage.getItem(`keyCodTutDoc`, pathCodTutDoc);
     //path de tutorias
     const pathTutoria = `Usuarios/${pathIdDoc}/Asignaturas/${pathIdAsigDoc}/Tutorias/${id}`
 
@@ -29,6 +34,63 @@ export default function Tutorias({
         const docRef = doc(database, `Usuarios/${pathIdDoc}/Asignaturas/${pathIdAsigDoc}/Tutorias/${id}`);
         deleteDoc(docRef);
     }
+
+    const [numEstudiantesInscritos, setNumEstudiantesInscritos] = React.useState(0);
+    async function numEstudiantesInscritosData() {
+        try {
+            const estudiantesRef = collection(database, 'Usuarios');
+            const estudiantesQuery = query(estudiantesRef, where("tipo", "==", "Estudiante"));
+            const estudiantesUnsubscribe = onSnapshot(estudiantesQuery, async (estudiantesSnapshot) => {
+                const promises = estudiantesSnapshot.docs.map(async (doc) => {
+                    const uid_Estudiante = doc.id;
+                    const tutoriaRef = collection(database, `/Usuarios/${uid_Estudiante}/AsignaturasEstudiante/${pathIdAsig}/TutoriasEstudiante/`);
+                    const tutoriaQuery = query(tutoriaRef, where("codigoTutoEst", "==", pathCodTutDoc));
+                    const tutoriaSnapshot = await getDocs(tutoriaQuery);
+                    const tutoriaData = tutoriaSnapshot.docs.map((tutoriaDoc) => tutoriaDoc.data());
+                    return tutoriaData.filter((tutoria) => tutoria.inscripcionTutoEst === 'true').length;
+                });
+                const results = await Promise.all(promises);
+                const totalEstudiantesInscritos = results.reduce((acc, curr) => acc + curr, 0);
+                setNumEstudiantesInscritos(totalEstudiantesInscritos);
+                console.log('numEstudiantesInscritosData > ', totalEstudiantesInscritos);
+            });
+            // Devolver la función de unsubscribe para limpiar el listener cuando sea necesario
+            return estudiantesUnsubscribe;
+        } catch (error) {
+            console.log('ERROR =>', error);
+        }
+    }
+
+    const [numTutoriasValidadas, setNumTutoriasValidadas] = React.useState(0);
+    async function numTutoriasValidas() {
+        try {
+            const estudiantesRef = collection(database, 'Usuarios');
+            const estudiantesQuery = query(estudiantesRef, where("tipo", "==", "Estudiante"));
+            const estudiantesUnsubscribe = onSnapshot(estudiantesQuery, async (estudiantesSnapshot) => {
+            const promises = estudiantesSnapshot.docs.map(async (doc) => {
+                const uid_Estudiante = doc.id;
+                const tutoriaRef = collection(database, `/Usuarios/${uid_Estudiante}/AsignaturasEstudiante/${pathIdAsig}/TutoriasEstudiante/`);
+                const tutoriaQuery = query(tutoriaRef, where("codigoTutoEst", "==", pathCodTutDoc));
+                const tutoriaSnapshot = await getDocs(tutoriaQuery);
+                const tutoriaData = tutoriaSnapshot.docs.map((tutoriaDoc) => tutoriaDoc.data());
+                return tutoriaData.filter((tutoria) => tutoria.validadaTutoEst === 'true').length;
+            });
+            const results = await Promise.all(promises);
+            const totalTutoriasValidadas = results.reduce((acc, curr) => acc + curr, 0);
+            setNumTutoriasValidadas(totalTutoriasValidadas);
+            console.log('numTutoriasValidas > ', totalTutoriasValidadas);
+        });
+            // Devolver la función de unsubscribe para limpiar el listener cuando sea necesario
+            return estudiantesUnsubscribe;
+        } catch (error) {
+            console.log('ERROR =>', error);
+        }
+    }
+
+    React.useEffect(() => { 
+        numEstudiantesInscritosData();  
+        numTutoriasValidas();
+    },[])
 
     const [isDeleteActive, setIsDeleteActive] = React.useState(false);
     const [modalVisible, setModalVisible] = useState(false);
@@ -87,8 +149,8 @@ export default function Tutorias({
                     <rn.View style={styleModal.modalView}>
                     <Ionicons name="information-circle-outline" size={26} color="#293774" style={{padding:10,  textAlign:'center'}} />
                         <rn.Text style={styleModal.modalTextTitle}>INFORMACIÓN!</rn.Text>
-                        <rn.Text style={styleModal.modalText}>Estudiantes inscritos: 11</rn.Text>
-                        <rn.Text style={styleModal.modalText}>Tutorias validadas:9 </rn.Text>
+                        <rn.Text style={styleModal.modalText}>Estudiantes inscritos: {numEstudiantesInscritos} </rn.Text>
+                        <rn.Text style={styleModal.modalText}>Tutorias validadas: {numTutoriasValidadas} </rn.Text>
 
                         <rn.Pressable
                         style={styleModal.buttonClose}
